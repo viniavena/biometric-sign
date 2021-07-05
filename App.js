e5fcce0491817608011e5a6d64b7c32696a3b73a
Vini Avena <admin@admins-MacBook-Air.local>
2021-07-05 17:29:35 -0300

    login started screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import LoggedOutScreen from './src/screens/LoggedOut';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LoggedOutScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +16,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- src/screens/LoggedOut.js
+++ src/screens/LoggedOut.js
@@ -1,8 +1,56 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-const LoggedOutScreen = () => {
-  return <View />;
-};
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>TouchMint</Text>
+      </View>
+      <Text style={styles.subTitle}>
+        Para entrar com sua impressão digital clique no botão abaixo!
+      </Text>
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.title}>Entrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
-export default LoggedOutScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 25,
+    textAlign: 'center',
+    fontWeight: '600',
+  },
+  header: {
+    width: '100%',
+    backgroundColor: '#00aa0080',
+    height: '18%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomRightRadius: 100,
+  },
+  subTitle: {
+    width: '80%',
+    color: '#00aa0080',
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: '600',
+    marginTop: 20,
+  },
+  loginButton: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#00aa0080',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 100,
+    top: '30%',
+  },
+});
