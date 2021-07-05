1b1a5af4d759a03b0fd0104e4987a1cbc316a629
Vini Avena <admin@admins-MacBook-Air.local>
2021-07-05 18:11:28 -0300

    verify auth supports

--- src/screens/LoggedOut.js
+++ src/screens/LoggedOut.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 import { Entypo, Ionicons } from '@expo/vector-icons';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function App() {
+  const [isBiometricAvailable, setIsBiometricAvailable] = useState(false);
+
+  // {FINGERPRINT, FACIAL_RECOGNITION, IRIS}.
+  //    A value of 1 indicates fingerprint support
+  //    2 indicates facial recognition support
+  //    3 indicates iris recognition support (Android-only)
+  //    Devices can support multiple authentication methods- i.e. [1,2]
+  //    If none are supported, this method returns an empty array.
+
+  async function verifySupports() {
+    const supported = await LocalAuthentication.hasHardwareAsync();
+    setIsBiometricAvailable(supported);
+
+    const supportedType =
+      await LocalAuthentication.supportedAuthenticationTypesAsync();
+
+    console.log(supportedType);
+  }
+
+  useEffect(() => {
+    verifySupports();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Entypo name="leaf" size={30} color="#fff" />
-        <Text style={styles.title}>TouchMint</Text>
+        <Text style={styles.title}>idMint</Text>
       </View>
+      {/* {
+          switch(supportedType){
+            case 
+          }
+      } */}
       <Text style={styles.subTitle}>
         Para entrar com sua impressão digital clique no botão abaixo!
       </Text>
